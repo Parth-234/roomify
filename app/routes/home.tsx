3cd7327410524316bffa67a5a1c885078e9c4e3b
Parth <143431694+Parth-234@users.noreply.github.com>
2026-03-02 14:48:19 +0530

    implemented drag and rop uploader

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -2,6 +2,8 @@ import type { Route } from "./+types/home";
 import Navbar from "../../components/Navbar";
 import {ArrowRight, ArrowUpRightIcon, Clock, Layers} from "lucide-react";
 import Button from "../../components/ui/Button";
+import Upload from "../../components/Upload";
+import {useNavigate} from "react-router";
 
 
 export function meta({}: Route.MetaArgs) {
@@ -11,7 +13,19 @@ export function meta({}: Route.MetaArgs) {
   ];
 }
 
+
+
 export default function Home() {
+
+    const navigate = useNavigate();
+    const handleUploadComplete = async (base64Image: string) => {
+        const newId = Date.now().toString()
+
+        navigate(`/visualizer/${newId}`)
+
+        return true;
+    }
+
   return(
       <div className="home">
         <Navbar />
@@ -28,7 +42,7 @@ export default function Home() {
             <p className="subtitle">
                 Roomify is an AI-first environment that helps you visualize, render, and ship architectural projects faster than ever.
             </p>
-            
+
             <div className="actions">
                 <a href="#upload" className="cta">
                     Start Building <ArrowRight className="icon"/>
@@ -49,7 +63,7 @@ export default function Home() {
                         <h3>Upload your floor plan</h3>
                         <p>supports JPEG, PNG, formats upto 10MB</p>
                     </div>
-                    <p>Upload Images</p>
+                    <Upload onComplete={handleUploadComplete} />
                 </div>
             </div>
         </section>
---
+++ app/routes/visualizer.$id.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const visualizerid = () => {
+  return (
+    <div>visualizerid</div>
+  );
+};
+
+export default visualizerid;
